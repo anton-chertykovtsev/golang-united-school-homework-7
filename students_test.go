c6e59213c6f1b0e85ce1e8a2623b56785c5738cd
anton-chertykovtsev <vader666@yandex.ru>
2022-06-04 00:54:45 +0300

    fix code quality

--- students_test.go
+++ students_test.go
@@ -22,6 +22,14 @@ func init() {
 
 // WRITE YOUR CODE BELOW
 
+func errorExpectedValue(e interface{}, g interface{}) string {
+	return fmt.Sprintf("Expected %v, but got %v", e, g)
+}
+
+func errorUnexpected(e interface{}) string {
+	return fmt.Sprintf("Unexpected error: %v", e)
+}
+
 var husband Person = Person{"John", "Doe", time.Date(1970, time.January, 1, 1, 1, 0, 0, time.Local)}
 var wife Person = Person{"Jane", "Doe", time.Date(1973, time.March, 3, 3, 3, 0, 0, time.Local)}
 var twin01 Person = Person{"Ann", "Doe", time.Date(2000, time.March, 5, 5, 10, 0, 0, time.Local)}
@@ -33,7 +41,7 @@ func TestPeople_Len(t *testing.T) {
 	var expect int = 2
 	r := ppl.Len()
 	if r != expect {
-		t.Errorf("Expected: %d, got %d", expect, r)
+		t.Errorf(errorExpectedValue(expect, r))
 	}
 }
 
@@ -43,7 +51,7 @@ func TestPeople_LessByBirthDay(t *testing.T) {
 	var expect bool = true
 	r := ppl.Less(a, b)
 	if r {
-		t.Errorf("Expected: %t, got %t", expect, r)
+		t.Errorf(errorExpectedValue(expect, r))
 	}
 }
 
@@ -53,7 +61,7 @@ func TestPeople_LessByFirstName(t *testing.T) {
 	var expect bool = false
 	r := ppl.Less(a, b)
 	if !r {
-		t.Errorf("Expected: %t, got %t", expect, r)
+		t.Errorf(errorExpectedValue(expect, r))
 	}
 }
 
@@ -63,7 +71,7 @@ func TestPeople_LessByLastName(t *testing.T) {
 	var expect bool = false
 	r := ppl.Less(a, b)
 	if !r {
-		t.Errorf("Expected: %t, got %t", expect, r)
+		t.Errorf(errorExpectedValue(expect, r))
 	}
 }
 
@@ -74,7 +82,7 @@ func TestPeople_Swap(t *testing.T) {
 	ppl.Swap(i, j)
 	for k, v := range ppl {
 		if v != expect[k] {
-			t.Errorf("Expected: %v, got %v", expect, ppl)
+			t.Errorf(errorExpectedValue(expect, ppl))
 		}
 	}
 
@@ -82,15 +90,16 @@ func TestPeople_Swap(t *testing.T) {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+var str string = "1 2 3\n4 5 6\n7 8 9"
+
 func TestMatrix_NewGoodMatrix(t *testing.T) {
-	var str string = "1 2 3\n4 5 6\n7 8 9"
 	var expect Matrix = Matrix{3, 3, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}}
 	var r, err = New(str)
 	if err != nil {
-		t.Errorf("Expected to get nil, but actual is %v", err)
+		t.Errorf(errorExpectedValue(nil, err))
 	}
 	if !reflect.DeepEqual(*r, expect) {
-		t.Errorf("Expected: %v, got %v", expect, *r)
+		t.Errorf(errorExpectedValue(expect, *r))
 	}
 }
 
@@ -98,7 +107,7 @@ func TestMatrix_NewBadColunsLength(t *testing.T) {
 	var str string = "1 2 3 5\n4 5 \n6 7 8 9 10"
 	var _, err = New(str)
 	if err == nil {
-		t.Errorf("Expected error: %v, but get nil", err)
+		t.Errorf(errorExpectedValue(err, nil))
 	}
 }
 
@@ -106,36 +115,33 @@ func TestMatrix_NewBadContent(t *testing.T) {
 	var str string = "1 2 3\n4 a 6\n7 8 9"
 	var _, err = New(str)
 	if err == nil {
-		t.Errorf("Expected error: %v, but get nil", err)
+		t.Errorf(errorExpectedValue(err, nil))
 	}
 }
 
 func TestMatrix_Rows(t *testing.T) {
-	var str string = "1 2 3\n4 5 6\n7 8 9"
 	var expect [][]int = [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 	var m, err = New(str)
 	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
+		t.Errorf(errorUnexpected(err))
 	}
 	if !reflect.DeepEqual(m.Rows(), expect) {
-		t.Errorf("Expected: %v, but got %v", expect, m.Rows())
+		t.Errorf(errorExpectedValue(expect, m.Rows()))
 	}
 }
 
 func TestMatrix_Cols(t *testing.T) {
-	var str string = "1 2 3\n4 5 6\n7 8 9"
 	var expect [][]int = [][]int{{1, 4, 7}, {2, 5, 8}, {3, 6, 9}}
 	var m, err = New(str)
 	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
+		t.Errorf(errorUnexpected(err))
 	}
 	if !reflect.DeepEqual(m.Cols(), expect) {
-		t.Errorf("Expected: %v, but got %v", expect, m.Rows())
+		t.Errorf(errorExpectedValue(expect, m.Rows()))
 	}
 }
 
 func TestMatrix_Set(t *testing.T) {
-	var str string = "1 2 3\n4 5 6\n7 8 9"
 	var tests = []struct {
 		r, c, v int
 		want bool
@@ -149,14 +155,14 @@ func TestMatrix_Set(t *testing.T) {
 	var responce bool
 	var m, err = New(str)
 	if err != nil {
-		t.Errorf("Unexpected error: %v", err)
+		t.Errorf(errorUnexpected(err))
 	}
 	for _, test := range tests {
 		tName := fmt.Sprintf("r:%d,c:%d,v:%d,w:%t",test.r,test.c,test.v,test.want)
 		t.Run(tName, func (t *testing.T) {
 			responce = m.Set(test.r, test.c, test.v)
 			if  responce != test.want {
-				t.Errorf("Expected: %v, but got %v", test.want, responce)
+				t.Errorf(errorExpectedValue(test.want, responce))
 			}
 		})
 	}
